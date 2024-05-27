f14a7a778ddc37e10d71aae167f52c15da226c64
Mahmudov Ramziddin <mahmudovramziddin25@gmail.com>
2024-05-27 21:43:31 +0500

    upload

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,6 @@ import App from "./App.jsx";
 import ShopContextProvider from "./Context/ShopContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
-}
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ShopContextProvider>
     <BrowserRouter>
