35963399f3764fed63160a95c3ba8db3eb83416a
Mahmudov Ramziddin <mahmudovramziddin25@gmail.com>
2024-06-10 12:15:37 +0500

    Fixed WebPage

--- src/Components/NewCollection/NewCollection.jsx
+++ src/Components/NewCollection/NewCollection.jsx
@@ -1,28 +1,51 @@
-import React, { useRef, useState } from "react";
+import React, { useLayoutEffect } from "react";
 import "./NewCollection.css";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import new_collection from "../Asset/new_collections";
 import Item from "../Item/Item";
 
 const NewCollection = () => {
+  // Register GSAP ScrollTrigger plugin
+  gsap.registerPlugin(ScrollTrigger);
+
+  // GSAP Animation
+  useLayoutEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".collections",
+        start: "top center",
+        end: "bottom 80%",
+        scrub: true,
+        markers: {
+          startColor: "transparent",
+          endColor: "transparent",
+        },
+      },
+    });
+
+    tl.from(".collections", {
+      scale: 1,
+      opacity: 0.4,
+      x: -300,
+      duration: 400,
+    }).to(".collections", { scale: 0.9, opacity: 1 });
+
+  }, []);
+
   return (
-    <>
-      <div>
-        <div className="collections">
-          {new_collection.map((item, i) => {
-            return (
-              <Item
-                key={i}
-                id={item.id}
-                name={item.name}
-                image={item.image}
-                new_price={item.new_price}
-                old_price={item.old_price}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </>
+    <div className="collections">
+      {new_collection.map((item, i) => (
+        <Item
+          key={i}
+          id={item.id}
+          name={item.name}
+          image={item.image}
+          new_price={item.new_price}
+          old_price={item.old_price}
+        />
+      ))}
+    </div>
   );
 };
 
